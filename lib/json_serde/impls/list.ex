70fe2b5b6a0d0696cc2ee76260f579e134fc0695
Brian Balser <brian.balser@gmail.com>
2020-03-30 11:59:15 -0400

    Swapped out custom Ok module for Brex.Result

--- lib/json_serde/impls/list.ex
+++ lib/json_serde/impls/list.ex
@@ -1,16 +1,16 @@
 defimpl JsonSerde.Serializer, for: List do
-  alias JsonSerde.Ok
   require JsonSerde
+  import Brex.Result.Mappers
 
   def serialize(list) do
-    Ok.transform(list, &JsonSerde.Serializer.serialize/1)
+    map_while_success(list, &JsonSerde.Serializer.serialize/1)
   end
 end
 
 defimpl JsonSerde.Deserializer, for: List do
-  alias JsonSerde.Ok
+  import Brex.Result.Mappers
 
   def deserialize(_, list) do
-    Ok.transform(list, &JsonSerde.Deserializer.deserialize(&1, &1))
+    map_while_success(list, &JsonSerde.Deserializer.deserialize(&1, &1))
   end
 end
--- mix.exs
+++ mix.exs
@@ -23,6 +23,7 @@ defmodule JsonSerde.MixProject do
   defp deps do
     [
       {:jason, "~> 1.2"},
+      {:brex_result, "~> 0.4.0"},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
   end
